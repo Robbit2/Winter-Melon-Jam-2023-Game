7657160146a4822bb05d1a951d4fd8db9b333173
achrostel <140854017+achrostel@users.noreply.github.com>
2023-12-28 17:18:19 +0100

    Change Dash and general Movement
    Make dash 8-directional; Add timer for dash timeout; Make movement weighty; Comment some stuff;

--- Scenes/Entities/player.gd
+++ Scenes/Entities/player.gd
@@ -1,75 +1,78 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprite
-@onready var coyote = $Coyote
+@onready var coyote_timer = $Coyote
+@onready var dash_timeout_timer = $DashTimeout
 
 @export var SPEED : float = 750.0
 @export var JUMP_VELOCITY : float = -800.0
 @export var AIR_JUMP_VELOCITY : float = -700.0
-@export var DOWN_PULL : float = 1.5
+## Gravity Strength Multiplier after Apex of Jump
+@export_range (1.0, 2.5) var DOWN_PULL : float = 1.5
+@export var DASH_SPEED_HORIZONTAL : float = 2000.0
+@export var DASH_SPEED_VERTICAL : float = 1000.0
+## How heavy the Movement feels (influences dash)
+@export_range (0.1, 10.0) var WEIGHT_FACTOR : float = 3.0
 
 var is_falling : bool = true
 var can_jump : bool = false
 var can_air_jump : bool = false
 
-var dashing = 0
+var can_dash : bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 2.0
-
-func dash(direction):
-	if direction:
-		dashing = 6000 * direction
-	velocity.x = dashing
-
-
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	if abs(dashing) > 5000:
-		if abs(dashing) == 5100:
-			velocity.x *= 0.1
 	
 	# gravity
 	if not is_on_floor():
-		if is_falling and coyote.is_stopped():
+		if is_falling and coyote_timer.is_stopped():
 			if velocity.y < 0:
 				velocity.y += gravity * delta
 			else:
 				velocity.y += gravity * delta * DOWN_PULL
-		elif coyote.is_stopped():
-			coyote.start()
+		elif coyote_timer.is_stopped():
+			coyote_timer.start()
 	
-	# reset double jump, can add in extra checks for charm in the future
+	# reset jump and fall state, can add in extra checks for charm in the future
 	if is_on_floor():
 		is_falling = false
 		can_air_jump = false
 		can_jump = true
 
-	# Handles Jump, if player is on the ground or has double jump let them jump
+	# Handles Jump
+	# if the player is not falling -> normal jump
+	# if the player is falling -> air jump
 	if Input.is_action_just_pressed("jump"):
 		if not is_falling and can_jump:
 			can_jump = false
-			is_falling = true
+			is_falling = true # a bit hacky, but we don't want coyote time to start on jump
 			can_air_jump = true
 			velocity.y = JUMP_VELOCITY
-		elif is_falling and can_air_jump:
+		elif is_falling and can_air_jump: # charm requirement can be placed here
 			can_air_jump = false
 			velocity.y = AIR_JUMP_VELOCITY
 
 	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED / 5)
+	# using move_toward for everything (not just deceleration) makes the movement more weighty
+	# it also allows us to just add velocity to the dash and have it move smoothly into normal speed
+	velocity.x = move_toward(velocity.x, direction * SPEED, SPEED / WEIGHT_FACTOR)
 
-	if Input.is_action_just_pressed("dash") and not dashing:
-		dash(direction)
-	
-	dashing = move_toward(dashing, 0, 50)
+	if Input.is_action_just_pressed("dash") and can_dash:
+		# up/down is inverted so DASH_SPEED_VERTICAL can have a positive sign
+		var dash_direction = Input.get_vector("left", "right", "up", "down").normalized()
+		dash_timeout_timer.start()
+		can_dash = false
+		velocity.x = sign(dash_direction.x) * DASH_SPEED_HORIZONTAL
+		velocity.y = sign(dash_direction.y) * DASH_SPEED_VERTICAL
 	
 	move_and_slide()
 
 func _on_coyote_timeout():
 	is_falling = true
 	can_air_jump = true
+
+# can add some sound cue or something when dash is ready
+func _on_dash_timeout():
+	can_dash = true
