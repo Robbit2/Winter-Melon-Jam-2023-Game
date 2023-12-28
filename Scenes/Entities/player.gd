31754643fc8f715d49385a5cffacf39c1a8630a3
achrostel <140854017+achrostel@users.noreply.github.com>
2023-12-28 16:32:01 +0100

    Tighten Jump Controls
    Add coyote time; Differentiate between normal jump and air jump; Add down pull after apex of jump

--- Scenes/Entities/player.gd
+++ Scenes/Entities/player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprite
+@onready var coyote = $Coyote
 
-const SPEED = 750.0
-const JUMP_VELOCITY = -400.0
-var jumps = 2
+@export var SPEED : float = 750.0
+@export var JUMP_VELOCITY : float = -800.0
+@export var AIR_JUMP_VELOCITY : float = -700.0
+@export var DOWN_PULL : float = 1.5
+
+var is_falling : bool = true
+var can_jump : bool = false
+var can_air_jump : bool = false
+
 var dashing = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 2.0
 
 func dash(direction):
 	if direction:
@@ -25,16 +32,30 @@ func _physics_process(delta):
 	
 	# gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if is_falling and coyote.is_stopped():
+			if velocity.y < 0:
+				velocity.y += gravity * delta
+			else:
+				velocity.y += gravity * delta * DOWN_PULL
+		elif coyote.is_stopped():
+			coyote.start()
 	
 	# reset double jump, can add in extra checks for charm in the future
 	if is_on_floor():
-		jumps = 2
+		is_falling = false
+		can_air_jump = false
+		can_jump = true
 
 	# Handles Jump, if player is on the ground or has double jump let them jump
-	if Input.is_action_just_pressed("jump") and is_on_floor() or Input.is_action_just_pressed("jump") and jumps > 0:
-		velocity.y = JUMP_VELOCITY
-		jumps = max(0, jumps - 1)
+	if Input.is_action_just_pressed("jump"):
+		if not is_falling and can_jump:
+			can_jump = false
+			is_falling = true
+			can_air_jump = true
+			velocity.y = JUMP_VELOCITY
+		elif is_falling and can_air_jump:
+			can_air_jump = false
+			velocity.y = AIR_JUMP_VELOCITY
 
 	var direction = Input.get_axis("left", "right")
 	if direction:
@@ -48,3 +69,7 @@ func _physics_process(delta):
 	dashing = move_toward(dashing, 0, 50)
 	
 	move_and_slide()
+
+func _on_coyote_timeout():
+	is_falling = true
+	can_air_jump = true
